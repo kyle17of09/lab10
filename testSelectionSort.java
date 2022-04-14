039b5eb0060995705674614f4e5ed3bdd2b07e18
Kyle McCarthy <kyle17of09@gmail.com>
2022-04-14 14:37:49 -0400

    commit testMixed() and testDuplicates() to branch2

---
+++ testSelectionSort.java
@@ -0,0 +1,47 @@
+public class testSelectionSort
+   {
+
+   SelectionSort sort = new SelectionSort();
+
+   @Test
+   public void testMixed(){
+
+       int[] arr = new int[5];
+       arr[0] = 8;
+       arr[1] = -9;
+       arr[2] = 7;
+       arr[3] = -10;
+       arr[4] = -2;
+
+       int[] Sortedarr = new int[5];
+       Sortedarr[0] = -10;
+       Sortedarr[1] = -9;
+       Sortedarr[2] = -2;
+       Sortedarr[3] = 7;
+       Sortedarr[4] = 8;
+
+       sort.basicSelectionSort(arr);
+       assert(Arrays.equals(arr, Sortedarr));
+   }
+
+   @Test
+   public void testDuplicates(){
+
+       int[] arr = new int[5];
+       arr[0] = 8;
+       arr[1] = 10;
+       arr[2] = 2;
+       arr[3] = 10;
+       arr[4] = 2;
+
+       int[] Sortedarr = new int[5];
+       Sortedarr[0] = 2;
+       Sortedarr[1] = 2;
+       Sortedarr[2] = 8;
+       Sortedarr[3] = 10;
+       Sortedarr[4] = 10;
+
+       sort.basicSelectionSort(arr);
+       assert(Arrays.equals(arr, Sortedarr));
+   }
+}
